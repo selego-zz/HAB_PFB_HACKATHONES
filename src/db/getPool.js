0f6f25d3a6192367426e4962f9d6ba90b0e5b40a
Rodrigo Masid <remd884@mailfence.com>
2024-09-24 18:19:31 +0200

    initdb corregido

--- src/db/getPool.js
+++ src/db/getPool.js
@@ -2,28 +2,22 @@
 import mysql from 'mysql2/promise';
 
 // Variables de entorno para la conexión con la base de datos.
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = process.env;
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
+
+//////
 
 // Variable en la que almacenamos la base de datos.
 let pool;
 
-/**
- * Función que retorna el pool de la conexión a la base de datos
- * @description Crea una conexión con la base de datos. En caso de que la base de datos especificada no exista, la crea.
- * @env {string} MYSQL_HOST - Host que alberga el servidor de la base de datos.
- * @env {string} MYSQL_USER - Nombre usuario de la conexión a la base de datos.
- * @env {string} MYSQL_PASSWORD - Password del usuario de la conexión a la base de datos.
- * @env {string} MYSQL_DB - Nombre de la base de datos.
- */
-
+// Función que retorna el pool de la conexión a la base de datos
 const getPool = async () => {
     try {
-        //Si no existe la conexión, la creamos
+        //Si no existe la conexión, la creamos.
         if (!pool) {
             pool = mysql.createPool({
                 host: MYSQL_HOST,
                 user: MYSQL_USER,
-                password: MYSQL_PASSWORD,
+                password: MYSQL_PASS,
                 timezone: 'Z',
             });
 
@@ -34,7 +28,7 @@ const getPool = async () => {
             pool = mysql.createPool({
                 host: MYSQL_HOST,
                 user: MYSQL_USER,
-                password: MYSQL_PASSWORD,
+                password: MYSQL_PASS,
                 database: MYSQL_DB,
                 timezone: 'Z',
             });
