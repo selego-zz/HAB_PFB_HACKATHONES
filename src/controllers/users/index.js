0fc7d479496c9e090e593943e974af1d2c492f35
Rodrigo Masid <remd884@mailfence.com>
2024-09-24 14:24:30 +0200

    index de controllers de users y pequeñas correcciones generales

---
+++ src/controllers/users/index.js
@@ -0,0 +1,13 @@
+import newUserController from './newUserController.js';
+import validateUserController from './validateUserController.js';
+import loginUserController from './loginUserController.js';
+import getOwnUserController from './getOwnUserController.js';
+import changePassController from './changePassController.js';
+
+export {
+    newUserController,
+    validateUserController,
+    loginUserController,
+    getOwnUserController,
+    changePassController,
+};
--- src/db/getPool.js
+++ src/db/getPool.js
@@ -1,20 +1,21 @@
-//dependencias
+// Importamos dependencias.
 import mysql from 'mysql2/promise';
 
-//Variables de entorno para la conexión con la base de datos
+// Variables de entorno para la conexión con la base de datos.
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = process.env;
 
-//variable en la que almacenamos la Base de Datos
+// Variable en la que almacenamos la base de datos.
 let pool;
 
 /**
  * Función que retorna el pool de la conexión a la base de datos
- * @description Crea una conexión con la base de datos. Encaso de que la base de datos especificada no exista, la crea.
+ * @description Crea una conexión con la base de datos. En caso de que la base de datos especificada no exista, la crea.
  * @env {string} MYSQL_HOST - Host que alberga el servidor de la base de datos.
  * @env {string} MYSQL_USER - Nombre usuario de la conexión a la base de datos.
  * @env {string} MYSQL_PASSWORD - Password del usuario de la conexión a la base de datos.
  * @env {string} MYSQL_DB - Nombre de la base de datos.
  */
+
 const getPool = async () => {
     try {
         //Si no existe la conexión, la creamos
