f09bde916c28112df515c4b3c8edb6c472912bb8
Rodrigo Masid <remd884@mailfence.com>
2024-09-24 12:13:34 +0200

    utilidades

---
+++ src/models/selectUserByIdModel.js
@@ -0,0 +1,28 @@
+// Importamos la función que devuelve una conexión con la base de datos.
+import getPool from '../../db/getPool.js';
+
+// Importamos los errores.
+import generateErrorUtil from '../utils/generateErrorUtil.js';
+
+//////
+
+// Función que realiza una consulta a la base de datos para seleccionar un usuario con un id dado.
+const selectUserByIdModel = async (userId) => {
+    const pool = await getPool();
+
+    // Comprobamos si hay algún usuario con el id proporcionado.
+    const [users] = await pool.query(
+        `SELECT id, username, avatar, email FROM users WHERE id = ?`,
+        [userId],
+    );
+
+    // Si no existe ningún usuario con ese id lanzamos un error.
+    if (users.length < 1) {
+        generateErrorUtil();
+    }
+
+    // Ya que el email no puede repetirse, el array de usuarios solo podrá contener un único usuario, que será un objeto en la posición 0. En caso de que no se haya encontrado a ningún usuario retornará undefined.
+    return users[0];
+};
+
+export default selectUserByIdModel;
---
+++ src/models/selectUserByUsernameModel.js
@@ -0,0 +1,20 @@
+// Importamos la función que devuelve una conexión con la base de datos.
+import getPool from '../../db/getPool.js';
+
+//////
+
+// Función que realiza una consulta a la base de datos para seleccionar un usuario con un nombre de usuario dado.
+const selectUserByUsernameModel = async (username) => {
+    const pool = await getPool();
+
+    // Comprobamos si hay algún usuario con el nombre de usuario proporcionado.
+    const [users] = await pool.query(
+        `SELECT id, username FROM users WHERE username = ?`,
+        [username],
+    );
+
+    // Ya que el nombre de usuario tiene que ser único, el array de usuarios solo podrá contener un único usuario, que será un objeto en la posición 0. En caso de que no se haya encontrado a ningún usuario retornará undefined.
+    return users[0];
+};
+
+export default selectUserByUsernameModel;
