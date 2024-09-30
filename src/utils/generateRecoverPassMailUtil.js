bc946d49aa72a832c9dd67a5dfe3909f0a9e18f8
Rodrigo Masid <remd884@mailfence.com>
2024-09-30 13:53:02 +0200

    mailrecoverutil text

--- src/utils/generateRecoverPassMailUtil.js
+++ src/utils/generateRecoverPassMailUtil.js
@@ -5,10 +5,10 @@ const generateRecoverPassMailUtil = (username, recoverPassCode) => {
     const mail = `<h2>Recuperación de contraseña</h2>
 <br>
 <h3>Hola ${username}</h3>
-<p>Estás recibiendo este correo electrónico por que alguien ha intentado recuperar tu contraseña de nuestro portal de Hackathons</p>
-<p>Si quieres cambiar tu contraseña, te basta pulsar en el siguiente enlace, e introducir una contraseña nueva. ¡No olvides poner repetir la nueva contraseña en la casilla de contraseña repetida, o no podrás cambiar tu contraseña!</p>
-<p>En enlace de recuperación de contraseña es: <a href='${recoveryLink}'>${recoveryLink}</a></p>
-<p>Si no has solicitado el cambio de contraseña, puedes ignorar este correo</p>    `;
+<p>Estás recibiendo este correo electrónico porque alguien ha intentado recuperar tu contraseña de nuestro portal <b>${process.env.APP_NAME}<b>.</p>
+<p>Si quieres cambiar tu contraseña, haz click en el siguiente enlace, e introduce una contraseña nueva. ¡No olvides repetir la nueva contraseña en la casilla de contraseña repetida, o no podrás cambiarla!</p>
+<p>El enlace de recuperación de contraseña es: <a href='${recoveryLink}'>${recoveryLink}</a>.</p>
+<p>Si no has solicitado el cambio de contraseña, puedes ignorar este correo.</p>    `;
 
     return mail;
 };
