951e2b20ec766dc3078d7a32fc5def13ef2401bf
PCEMI\emili <emiliosansanchez@gmail.com>
2024-10-08 11:42:10 +0200

    pagina de registro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,10 @@ import { Routes, Route } from 'react-router-dom';
 // Importamos los componentes
 import Header from './components/Header.jsx';
 import HomePage from './pages/HomePage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
 import CreateHackathonPage from './pages/CreateHackathonPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
+
 import PruebaPage from './pages/pruebaPage.jsx';
 
 //Importamos otras funciones
@@ -15,21 +17,25 @@ import Footer from './components/Footer.jsx';
 ////////////////////////////////////////////
 
 const App = () => {
-  return (
-    <section className='flex flex-col gap-4 h-screen'>
-      <Toaster position='top-center' toastOptions={{ duration: 3000 }} />
-      <Header />
-      <section className='flex-grow'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/prueba' element={<PruebaPage />} />
-          <Route path='/hackathons/create' element={<CreateHackathonPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </section>
-      <Footer />
-    </section>
-  );
+    return (
+        <section className="flex flex-col gap-4 h-screen">
+            <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+            <Header />
+            <section className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="users/register" element={<RegisterPage />} />
+                    <Route path="/prueba" element={<PruebaPage />} />
+                    <Route
+                        path="/hackathons/create"
+                        element={<CreateHackathonPage />}
+                    />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </section>
+            <Footer />
+        </section>
+    );
 };
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,86 +6,86 @@ const { VITE_APP_NAME } = import.meta.env;
 //////
 
 const Header = () => {
-  const authContext = useContext(AuthContext);
-  if (!authContext) {
-    return null;
-  }
+    const authContext = useContext(AuthContext);
+    if (!authContext) {
+        return null;
+    }
 
-  const { authUser, isAdmin, isDeveloper, isOrganizer, authLogoutState } =
-    authContext;
+    const { authUser, isAdmin, isDeveloper, isOrganizer, authLogoutState } =
+        authContext;
 
-  return (
-    <header className='bg-blanco text-negro py-4 shadow-md'>
-      <div className='container mx-auto flex flex-col sm:flex-row justify-between items-center'>
-        <NavLink to='/' className='text-2xl font-bold mb-2 sm:mb-0'>
-          <h1>{VITE_APP_NAME}</h1>
-        </NavLink>
+    return (
+        <header className="bg-blanco text-negro py-4 shadow-md">
+            <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
+                <NavLink to="/" className="text-2xl font-bold mb-2 sm:mb-0">
+                    <h1>{VITE_APP_NAME}</h1>
+                </NavLink>
 
-        <nav className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 mb-2 sm:mb-0'>
-          {isDeveloper() && (
-            <NavLink
-              to='/events'
-              className='bg-verdeclaro text-negro py-2 px-4 rounded-3xl hover:bg-verdemarino'
-            >
-              <button>Eventos</button>
-            </NavLink>
-          )}
-          {isOrganizer() && (
-            <NavLink
-              to='/hackathons/create'
-              className='bg-verdemarino text-negro py-2 px-4 rounded-3xl hover:bg-verdeagua'
-            >
-              <button>Crea un Hackathon</button>
-            </NavLink>
-          )}
-          {isAdmin() && (
-            <NavLink
-              to='/user-list'
-              className='bg-verdeagua text-negro py-2 px-4 rounded-3xl hover:bg-verdemarino'
-            >
-              <button>Listado de usuarios</button>
-            </NavLink>
-          )}
-        </nav>
+                <nav className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 mb-2 sm:mb-0">
+                    {isDeveloper() && (
+                        <NavLink
+                            to="/events"
+                            className="bg-verdeclaro text-negro py-2 px-4 rounded-3xl hover:bg-verdemarino"
+                        >
+                            <button>Eventos</button>
+                        </NavLink>
+                    )}
+                    {isOrganizer() && (
+                        <NavLink
+                            to="/hackathons/create"
+                            className="bg-verdemarino text-negro py-2 px-4 rounded-3xl hover:bg-verdeagua"
+                        >
+                            <button>Crea un Hackathon</button>
+                        </NavLink>
+                    )}
+                    {isAdmin() && (
+                        <NavLink
+                            to="/user-list"
+                            className="bg-verdeagua text-negro py-2 px-4 rounded-3xl hover:bg-verdemarino"
+                        >
+                            <button>Listado de usuarios</button>
+                        </NavLink>
+                    )}
+                </nav>
 
-        <nav className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4'>
-          {!authUser ? (
-            <>
-              <NavLink
-                to='/login'
-                className='bg-casiblanco text-negro py-2 px-4 rounded-3xl hover:bg-verdeclaro'
-              >
-                <button>Iniciar sesión</button>
-              </NavLink>
-              <NavLink
-                to='/register'
-                className='bg-verdeclaro text-negro py-2 px-4 rounded-3xl hover:bg-azuloscuro hover:text-blanco'
-              >
-                <button>Registrarse</button>
-              </NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink
-                to='/profile'
-                className='bg-blanco text-negro py-2 px-4 rounded-3xl hover:bg-casiblanco'
-              >
-                <button>Perfil</button>
-              </NavLink>
-              <NavLink to='/'>
-                <button
-                  onClick={authLogoutState}
-                  className='bg-casiblanco text-negro py-2 px-4 rounded-3xl hover:bg-rojoclaro'
-                >
-                  Cerrar sesión
-                </button>
-              </NavLink>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
-  );
+                <nav className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+                    {!authUser ? (
+                        <>
+                            <NavLink
+                                to="/login"
+                                className="bg-casiblanco text-negro py-2 px-4 rounded-3xl hover:bg-verdeclaro"
+                            >
+                                <button>Iniciar sesión</button>
+                            </NavLink>
+                            <NavLink
+                                to="/users/register"
+                                className="bg-verdeclaro text-negro py-2 px-4 rounded-3xl hover:bg-azuloscuro hover:text-blanco"
+                            >
+                                <button>Registrarse</button>
+                            </NavLink>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink
+                                to="/profile"
+                                className="bg-blanco text-negro py-2 px-4 rounded-3xl hover:bg-casiblanco"
+                            >
+                                <button>Perfil</button>
+                            </NavLink>
+                            <NavLink to="/">
+                                <button
+                                    onClick={authLogoutState}
+                                    className="bg-casiblanco text-negro py-2 px-4 rounded-3xl hover:bg-rojoclaro"
+                                >
+                                    Cerrar sesión
+                                </button>
+                            </NavLink>
+                        </>
+                    )}
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
