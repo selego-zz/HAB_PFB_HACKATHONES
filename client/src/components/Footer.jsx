3487fcf606c6a485e1a62a712c2c1eedaa5250a2
Rodrigo Masid <remd884@mailfence.com>
2024-10-08 19:26:16 +0200

    homepage de amanda

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 const { VITE_APP_NAME } = import.meta.env;
 
 const Footer = () => {
-  return (
-    <footer className=' bg-green-300'>
-      {VITE_APP_NAME} &copy; Grupo B 2024
-    </footer>
-  );
+    return (
+        <footer className="bg-azuloscuro">
+            {VITE_APP_NAME} &copy; Grupo B 2024
+        </footer>
+    );
 };
 
 export default Footer;
