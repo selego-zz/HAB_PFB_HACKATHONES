6787a74e2652f49d2ec735439f0fcba73a996be2
Amanda González <amandagc@outlook.es>
2024-10-09 12:32:40 +0200

    cambios

--- client/src/pages/users/HomePage.jsx
+++ client/src/pages/users/HomePage.jsx
@@ -1,8 +1,5 @@
 import { useDocumentTitle } from '../../hooks/index.js'; // Importamos hooks
-
 import { useNavigate } from 'react-router-dom';
-
-//////
 
 const HomePage = () => {
     useDocumentTitle('Inicio'); // Título de pestaña
@@ -15,23 +12,27 @@ const HomePage = () => {
     const handleParticipantesClick = () => {
         navigate('NotFoundPage');
     };
+
     return (
         <main>
             <div className="hidden sm:block">
-                <h1 className="  bg-azuloscuro h-32 font-semibold text-4xl text-blanco font-jost flex justify-center items-center p-10 text-center">
+                <h1 className="bg-azuloscuro h-32 font-semibold text-4xl text-blanco font-jost flex justify-center items-center p-10 text-center">
                     Participa en los mejores hackathones en línea y
                     presenciales.
                 </h1>
             </div>
 
-            <div className="bg-[url('/assets/images/back-banner.jpg')] inset-0 bg-cover bg-center z-0">
-                <div className="relative z-10 bg-blanco bg-opacity-90 p-8 max-w-full mx-auto rounded-lg shadow-lg">
-                    <div className="flex flex-col items-center justify-center p-3 sm:items-start sm😛t-20">
-                        <h2 className="font-semibold text-3xl font-jost sm😛l-20 mt-10 sm:mt-0">
+            {/* Nueva sección para la imagen y el texto */}
+            <div className="relative">
+                <div className="bg-[url('/assets/images/back-banner.jpg')] bg-cover sm:absolute sm:left-1/2 sm:top-0 sm:w-1/2 h-full"></div>
+
+                <div className="bg-opacity-0 sm:w-1/2 sm:pr-10">
+                    <div className="flex flex-col items-center justify-center p-3 sm:items-start sm:pt-20">
+                        <h2 className="font-semibold text-3xl font-jost sm:pl-20 mt-10 sm:mt-0">
                             El hogar de los hackathones.
                         </h2>
                     </div>
-                    <div className="flex flex-col items-center justify-center sm:items-start ml-10 mr-10 sm:ml-24 sm:w-2/6">
+                    <div className="flex flex-col items-center justify-center sm:items-start ml-10 mr-10 sm:ml-24 sm:w-2/3">
                         <p className="font-jost font-medium text-center sm:text-left">
                             Donde las organizaciones y los desarrolladores se
                             unen para construir, inspirar e innovar.
@@ -48,8 +49,7 @@ const HomePage = () => {
                             onClick={handleParticipantesClick}
                             className="bg-verdemarino text-azuloscuro font-semibold font-jost h-12 w-72 rounded-3xl mt-10 text-2xl sm:ml-32 transition duration-100 hover:bg-azuloscuro hover:text-blanco"
                         >
-                            Participantes
-                            <span className="ml-16">→</span>
+                            Participantes<span className="ml-16">→</span>
                         </button>
                     </div>
                 </div>
