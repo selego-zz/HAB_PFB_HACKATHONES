e3888722ce3897482c6fc2be498116668680a0a8
Rodrigo Masid <remd884@mailfence.com>
2024-10-12 21:58:08 +0200

    hackdetails1.0 + avatares y logos

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <footer className="bg-azuloscuro h-52 flex flex-col items-center justify-between">
             <img
-                src="/Logo/Hack-a-ton.png"
-                alt="Logo"
+                src="/logo/app-logo2.png"
+                alt="Logo de la página."
                 className="h-28 mt-10 "
             />
             <p className="font-jost font-semibold mb-2">
