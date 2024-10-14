7c1888d05d93187460e12ad7fd320a710ed7c954
Rodrigo Masid <remd884@mailfence.com>
2024-10-14 13:34:30 +0200

    404

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -13,25 +13,27 @@ const NotFoundPage = () => {
     };
 
     return (
-        <main className="flex items-center justify-center min-h-screen text-center p-6">
-            <div className="p-8 rounded-lg bg-azuloscuro shadow-lg max-w-lg mx-auto">
+        <main className="flex items-center justify-center min-h-fit text-center p-6">
+            <div className="p-8 rounded-lg bg-azuloscuro shadow-2xl max-w-lg mx-auto">
                 <h1 className="text-6xl font-bold text-rojoclaro mb-4">404</h1>
                 <p className="text-xl mb-4 text-blanco">
                     ¡Ups! No hemos encontrado la página que buscas.
                 </p>
-                <pre className="text-left bg-amarillo rounded-lg p-4 mb-6">
-                    <code>
-                        {`function notFound() {
+                <div className="overflow-auto mb-6">
+                    <p className="text-left bg-amarillo rounded-lg p-4">
+                        <code>
+                            {`function notFound() {
     console.log("Esta página no existe...");
     return "404 - Page Not Found";
 }
 
 notFound();`}
-                    </code>
-                </pre>
+                        </code>
+                    </p>
+                </div>
                 <button
                     onClick={goHome}
-                    className="bg-azuloscuro text-blanco hover:bg-amarillo hover:text-negro font-semibold py-2 px-6 rounded-full transition duration-300"
+                    className="bg-azuloscuro text-blanco hover:bg-amarillo hover:text-negro text-xl font-semibold py-2 px-6 rounded-full transition duration-300"
                 >
                     Regresar a Inicio
                 </button>
