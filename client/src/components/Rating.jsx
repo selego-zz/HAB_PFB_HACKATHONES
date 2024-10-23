397744402376258683b96164d0e9db9962b4a554
Rodrigo Masid <remd884@mailfence.com>
2024-10-23 19:56:33 +0200

    confirm por sweetalert

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -4,19 +4,38 @@ import PropTypes from 'prop-types';
 import useHackathons from '../hooks/useHackathons';
 import { useState } from 'react';
 
+import Swal from 'sweetalert2';
+
+///////
+
 const MakeStar = (star, rating, setRating, hackathonId, isDeveloper) => {
     const { updateRating } = useHackathons();
     return (
         <span
             key={rating}
             className="hover:text-6xl cursor-pointer"
-            onClick={() => {
-                if (
-                    isDeveloper &&
-                    confirm('¿Está seguro de que desea cambiar la puntuación?')
-                ) {
-                    updateRating(hackathonId, rating);
-                    setRating(rating);
+            onClick={async () => {
+                if (isDeveloper) {
+                    const confirmChangeRating = await Swal.fire({
+                        title: 'Cambiar valoración',
+                        text: '¿Estás seguro de querer cambiar tu valoración de este hackathon?',
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Sí, quiero cambiarla',
+                        cancelButtonText: 'Cancelar',
+                    });
+
+                    if (confirmChangeRating.isConfirmed) {
+                        updateRating(hackathonId, rating);
+                        setRating(rating);
+                        Swal.fire(
+                            '¡Hecho!',
+                            'La puntuación ha sido actualizada.',
+                            'success',
+                        );
+                    }
                 }
             }}
         >
